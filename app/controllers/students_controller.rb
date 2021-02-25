3b080480b50f02a9da88c37cea491afd83aa7848
Doug <doug.kerzner@gmail.com>
2021-02-25 09:55:33 -0500

    commit tree f16ed1ec655d52cd143f88a02e7a09dc88d8f7af

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,14 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new 
+    @student = Student.new 
+  end 
+
+  def create 
+    student_params = params.require(:student).permit(:first_name, :last_name)
+    new_student = Student.create(student_params)
+    redirect_to student_path(new_student)
+  end 
+
 end
